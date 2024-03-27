6ad0ea28b1fd6547345897260dfb2f01b2443b49
Raksha Adhikari <156924801+Thisismygirlrakshaforsure@users.noreply.github.com>
2024-03-27 12:02:30 +0545

    Create (p)or(c).php

---
+++ (p)or(c).php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+</head>
+<body>
+    <?php
+    x=5;
+    for ($i=1; $i<5; $i++)
+    {
+        if ($x%$i==0)
+        {
+            $c++;
+        }
+       if ($c>2) 
+    {
+      echo "prime num"  
+    }
+    else if
+    echo "composite num"
+}
+</body>
+?>
